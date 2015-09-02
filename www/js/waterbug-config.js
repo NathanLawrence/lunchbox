05df9c79c843cd7bd8efcba2ee0b32b3b8438784
Nathan Lawrence <nathan.lawrence.barrick@gmail.com>
2015-09-02 11:38:01 -0500

    Further KBIA Customization

--- www/js/waterbug-config.js
+++ www/js/waterbug-config.js
@@ -5,20 +5,34 @@ var elementPadding = 40; // padding around the logo and credit text
 // logo configuration
 // the name of the logo object should match the value of the corresponding radio button in the HTML.
 var logos = {
-    'lunchbox': {
-        whitePath: '../img/icon-lunchbox-white.svg', // path to white logo
-        blackPath: '../img/icon-lunchbox-black.svg', // path to black logo
-        w: 100, // width of logo
-        h: 80, // height of logo
-        display: 'Lunchbox'
+    'kbia-neutral': {
+        whitePath: '../img/KBIA_main_WHITE.svg', // path to white logo
+        blackPath: '../img/KBIA_main_black.svg', // path to black logo
+        w: 137, // width of logo
+        h: 55, // height of logo
+        display: 'KBIA Neutral'
     },
-    'socializr': {
-        whitePath: '../img/icon-socializr-white.svg',
-        blackPath: '../img/icon-socializr-black.svg',
-        w: 150,
-        h: 51,
-        display: 'Socializr'
+    'kbia-blue': {
+        whitePath: '../img/KBIA_main_BLUE.svg',
+        blackPath: '../img/KBIA_MAIN_BLUE.svg',
+        w: 137,
+        h: 55,
+        display: 'KBIA Blue'
     }
+    // 'lunchbox': {
+    //     whitePath: '../img/icon-lunchbox-white.svg', // path to white logo
+    //     blackPath: '../img/icon-lunchbox-black.svg', // path to black logo
+    //     w: 100, // width of logo
+    //     h: 80, // height of logo
+    //     display: 'Lunchbox'
+    // },
+    // 'socializr': {
+    //     whitePath: '../img/icon-socializr-white.svg',
+    //     blackPath: '../img/icon-socializr-black.svg',
+    //     w: 150,
+    //     h: 51,
+    //     display: 'Socializr'
+    // }
 };
 
 // logo opacity for colors
@@ -35,7 +49,7 @@ var fontShadowOffsetY = 0; // font shadow offset y
 var fontShadowBlur = 10; // font shadow blur
 
 // copyright options
-var orgName = 'Your News Organization';
+var orgName = 'KBIA';
 var freelanceString = 'for ' + orgName;
 
 var copyrightOptions = {
@@ -83,7 +97,7 @@ var copyrightOptions = {
 
 // app load defaults
 var currentCrop = 'twitter'; // default crop size
-var currentLogo = 'lunchbox'; // default logo slug
+var currentLogo = 'kbia-neutral'; // default logo slug
 var currentLogoColor = 'white'; // default logo color
 var currentTextColor = 'white'; // default text color
 var defaultImage = '../img/test-kitten.jpg'; // path to image to load as test image
